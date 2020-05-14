1b9e4925fbd29e93b68e44d5f820f09f971ff3b3
Felix <fgalperin@udesa.edu.ar>
2020-05-13 22:22:22 -0300

    Hecho menos q a medias el tema de ruteo y controller
    Falta q me conteste gonza y lo termino

--- routes/rutasGral.js
+++ routes/rutasGral.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('actoresdetalles',)
+router.get('actoresdetalles',require(/actores/actoresdetalles))
 
 module.exports = router;
- 
