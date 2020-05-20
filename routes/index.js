49b8d5132bb09acc91905202371d1dd9934b56f6
Felix <fgalperin@udesa.edu.ar>
2020-05-20 19:15:27 -0300

    devuelta

--- routes/index.js
+++ routes/index.js
@@ -5,60 +5,17 @@ const controller = require('../Controllers/controladorViews')
     
 
 router.get("/", controller.home);
-router.get("/detalles", controller.detalles);
+router.get("/popular", controller.popular);
+router.get("/upcoming", controller.upcoming);
+router.get("/actors", controller.actors);
+router.get("/generos", controller.generos);
+router.get("/favoritos", controller.favoritos);
+router.get("/login", controller.login);
 
 
 // HTTP://localhost:3000/actoresdetalles
 
 
 router.get('/actoresdetalles', controller.actoresdetalles);
-
-router.get("/actors", function(){
-    return view('actors');
-});
-
-router.get("/buscador", function(){
-    return view('buscador');
-});
-
-router.get("/detalles", function(){
-    return view('detalles');
-});
-
-router.get("/error", function(){
-    return view('error');
-});
-
-router.get("/favoritos", function(){
-    return view('favoritos');
-});
-
-router.get("/generos", function(){
-    return view('generos');
-});
-
-router.get("/index", function(){
-    return view('index');
-});
-
-router.get("/login", function(){
-    return view('login');
-});     
-
-router.get("/nuevo", function(){
-    return view('nuevo');
-});
-
-router.get("/popular", function(){
-    return view('popular');
-});
-
-router.get("/tiposdegeneros", function(){
-    return view('tiposdegeneros');
-});
-
-router.get("/upcoming", function(){
-    return view('upcoming');
-});
 
 module.exports = router;
