96c40d92dab9815383f4c86c28fff14356e65f03
Federico <feder.davola@gmail.com>
2020-06-01 11:07:23 -0300

    correcion Models

--- database/models/resenas.js
+++ database/models/resenas.js
@@ -1,14 +1,14 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../config/config');
 
-const Resenas = sequelize.define('resenas', {
-    id: Sequelize.DECIMAL,
-    peliculaId: Sequelize.DECIMAL,
-    usuarioId: Sequelize.DECIMAL,
+const Resena = sequelize.define('resenas', {
+    id: Sequelize.INTEGER,
+    peliculaId: Sequelize.INTEGER,
+    usuarioId: Sequelize.INTEGER,
     textoResena: Sequelize.STRING,
     puntaje: Sequelize.DECIMAL,
     createdAt: Sequelize.DATE,
     updatedAt: Sequelize.DATE,
 });
 
-module.exports = Resenas;
+module.exports = Resena;
--- database/models/usuarios.js
+++ database/models/usuarios.js
@@ -1,12 +1,12 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../config/config');
 
-const Usuarios = sequelize.define('usuarios', {
-    id: Sequelize.DECIMAL,
+const Usuario = sequelize.define('usuarios', {
+    id: Sequelize.INTEGER,
     nombreCompleto: Sequelize.STRING,
     email: Sequelize.STRING,
     password: Sequelize.STRING,
     fechaNacimiento: Sequelize.DATE,
 });
 
-module.exports = Usuarios;
+module.exports = Usuario;
